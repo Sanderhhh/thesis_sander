7a9de8d66954ed1603c08bbc8f580b27d56ff6d6
Sanderhhh <78415995+Sanderhhh@users.noreply.github.com>
2023-04-11 17:06:16 +0200

    Installed gpt2

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,12 @@
-print("Hello, world!")
-print("This is the tutorial branch.")
+from transformers import pipeline, set_seed
+
+generator = pipeline('text-generation', model='gpt2')
+
+set_seed(42)
+
+dict_list = generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5)
+
+num = 0
+for dict in dict_list:
+    num += 1
+    print("Sequence number " + str(num) + ": " + dict["generated_text"])
